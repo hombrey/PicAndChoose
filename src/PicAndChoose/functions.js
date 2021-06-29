47e0c93fb315564836a9e532fed16a8ab664db8e
Hombrey Escalante <hombrey.escalante@gmail.com>
2021-06-29 15:07:03 +0700

    selects and evaluates

--- src/PicAndChoose/functions.js
+++ src/PicAndChoose/functions.js
@@ -2,12 +2,26 @@
 "use strict";
 let bgX;
 let scaleX, scaleY;
-let imgSet;
+let promptSet;
+let choices;
 let imgIndex=0;
 let assetDir, sourceDir;
 let mainImg;
 let pickSound;
+let tingSound;
+let errSound;
+let activeNum=1;
 //}}}variable declarations
+
+
+//{{{class declarations
+class SmartString  {
+    constructor(text,ans){
+        this.txt = text;
+        this.ans = ans;
+    } //constructor
+} //class SmartString
+//}}}class declarations
 
 //{{{event listeners
 window.onload = initWin();
@@ -15,8 +29,12 @@ window.addEventListener("resize", initWin);
 window.addEventListener("keydown", evalKeyDown, false); //capture keypress on bubbling (false) phase
 function evalKeyDown(evnt) {
     let keyPressed = evnt.keyCode;
-    console.log ("keyDn: ",keyPressed);
+    //console.log ("keyDn: ",keyPressed);
     switch (keyPressed) {
+        case 49 : evalChosen(1); break; //key: 1
+        case 50 : evalChosen(2); break; //key: 2
+        case 51 : evalChosen(3); break; //key: 3
+        case 52 : evalChosen(4); break; //key: 4
         case 38 : viewNextImg(-1); 
                   break; //key: <up>
         case 40 : viewNextImg(1); 
@@ -47,43 +65,64 @@ setTimeout (function() { //set delay before calculating drawable parameters
 
     scaleX = bgX.clientWidth/bgX.naturalWidth;
     scaleY = bgX.clientHeight/bgX.naturalHeight;
-    imgSet = [
-                "1_bread1.jpg",
-                "2_cookie1.jpg",
-                "3_bread2.jpg",
-                "4_cookie2.jpg",
-                "5_cookie3.jpg",
-                "6_bread3.jpg",
-                "7_bread4.jpg",
-                "8_cookie4.jpg",
-                "9_bread5.jpg",
-                "10_cookie5.jpg",
-    ""]; //imgSet
 
     pickSound = new sound(sourceDir+"wav/pick.mp3");
-}, 3);//setTimeOut (function()
+    tingSound = new sound(sourceDir+"wav/ting.mp3");
+    errSound = new sound(sourceDir+"wav/err.mp3");
+
+}, 10);//setTimeOut (function()
 };//document.getElementById ... wait for element before loading
 } //function initWin()
 
-//}}}window init
+//}}}initializations
 
 //{{{handler functions
 function viewNextImg(inc) {
     imgIndex=imgIndex+inc;
-    if (imgIndex<0) imgIndex = imgSet.length-2;
-    else if (imgSet[imgIndex]=="") imgIndex = 0;
+   
+    resetPosition(); 
+    if (imgIndex<0) imgIndex = promptSet.length-2;
+    else if (promptSet[imgIndex].txt=="") imgIndex = 0;
 
-    let imgSrc =(assetDir+imgSet[imgIndex]);
+    let imgSrc =(assetDir+promptSet[imgIndex].txt);
     //console.log("index: "+imgIndex);
     //console.log("image: "+imgSrc);
 
     pickSound.start();
     mainImg.src = imgSrc;
 } //function vewNextImg(inc)
+function evalClick(clicked_id) {
+    let extractIdNum = (clicked_id.replace("choice",""));
+    let clickedNum = parseInt(extractIdNum);
+    //console.log ("clicked:"+clickedNum); 
+    evalChosen(clickedNum);
+} //function evalClick(clicked_id)
+
+function evalChosen(numChosen) {
+    resetPosition(); //reset previous active number before switching
+    activeNum = numChosen;
+    choices[numChosen-1].style.top= '43vh';
+    choices[numChosen-1].style.left= '43vw';
+
+    if (numChosen == promptSet[imgIndex].ans) {
+        tingSound.start();
+    }// if (numChosen == promptSet)
+    else {
+        errSound.start();
+        setTimeout (function () {
+            resetPosition(); 
+        }, 300); //setTimeOut
+    } // else [of if numChosen == promptSet]
+
+} //function evalChosen(numChosen)
 
 //}}}handler functions
 
 //{{{helper functions
+function resetPosition() {
+            choices[activeNum-1].style.top= choices[activeNum-1].resetY;
+            choices[activeNum-1].style.left= choices[activeNum-1].resetX; 
+} //function resetPosition()
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
--- src/PicAndChoose/genArray.sh
+++ src/PicAndChoose/genArray.sh
@@ -2,11 +2,13 @@
 ls $1 > list.txt
 
 echo $1 > array.txt
+echo "    ansSet = [" >> array.txt
 input="./list.txt"
 while IFS= read -r line
 do
-    echo "        \"$line\"," >> array.txt
+    echo "        new SmartString(\"$line\",1)," >> array.txt
 done < "$input"
+    echo "    new SmartString(\"\",1)];" >> array.txt
 rm list.txt
 
 
